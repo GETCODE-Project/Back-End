4be77b98193f8f57ab8d8cde71f3d8ca0e72b493
KMK_HM <kyun9151@gmail.com>
2023-12-19 17:37:17 +0900

    init project

---
+++ src/main/java/com/getcode/GetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.getcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getcode/GetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
