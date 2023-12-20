8a74a1e9868e9dbf9a0129879dab22dc13288173
KMK_HM <kyun9151@gmail.com>
2023-12-20 16:20:02 +0900

    feat: Memeber Entity 설정

--- src/main/java/com/getcode/domain/member/Member.java
+++ src/main/java/com/getcode/domain/member/Member.java
@@ -1,8 +1,39 @@
 package com.getcode.domain.member;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+
+
+
 }
