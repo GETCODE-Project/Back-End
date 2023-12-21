398d72a5ce0d47cb0233a3dfe21abeb8df02b4e0
KMK_HM <kyun9151@gmail.com>
2023-12-21 13:37:16 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/com/getcode/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.getcode.repository;
+
+import com.getcode.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
