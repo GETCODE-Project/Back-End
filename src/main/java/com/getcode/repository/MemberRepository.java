150538aa749ab581b11bf0fe40286a682d40bbf6
KMK_HM <kyun9151@gmail.com>
2023-12-26 11:53:08 +0900

    feat: MemberRespository method 추가

--- src/main/java/com/getcode/repository/MemberRepository.java
+++ src/main/java/com/getcode/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.getcode.repository;
 
 import com.getcode.domain.member.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String nickname);
 }
