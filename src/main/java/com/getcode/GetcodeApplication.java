6a7881ee1c870698b182be6caf202c21f35014d7
KMK_HM <kyun9151@gmail.com>
2023-12-26 11:57:10 +0900

    feat: BaseTimeEntity 추가

--- src/main/java/com/getcode/GetcodeApplication.java
+++ src/main/java/com/getcode/GetcodeApplication.java
@@ -2,8 +2,10 @@ package com.getcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GetcodeApplication {
     public static void main(String[] args) {
 
--- src/main/java/com/getcode/domain/member/Member.java
+++ src/main/java/com/getcode/domain/member/Member.java
@@ -1,5 +1,6 @@
 package com.getcode.domain.member;
 
+import com.getcode.domain.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,13 +9,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Member {
+public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
@@ -31,4 +33,11 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Builder
+    public Member(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
