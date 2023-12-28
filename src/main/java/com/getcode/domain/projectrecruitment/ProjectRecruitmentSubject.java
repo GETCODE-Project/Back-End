2c2b8740ba0b743930845a93c98779b4b4441158
jaeuk kim <wodnr76@naver.com>
2023-12-28 21:34:49 +0900

    Rename: enumerated 어노테이션 추가

--- src/main/java/com/getcode/domain/projectrecruitment/ProjectRecruitmentSubject.java
+++ src/main/java/com/getcode/domain/projectrecruitment/ProjectRecruitmentSubject.java
@@ -1,2 +1,28 @@
-package com.getcode.domain.projectrecruitment;public class ProjectRecruitmentSubject {
+package com.getcode.domain.projectrecruitment;
+
+
+import com.getcode.domain.common.Subject;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ProjectRecruitmentSubject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_recruitment_subject_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "subject_name")
+    private Subject subject;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_recruitment_id")
+    private ProjectRecruitment projectRecruitment;
+
 }
