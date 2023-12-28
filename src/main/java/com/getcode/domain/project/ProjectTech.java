6206a0c71ecc8fda31f3c7993920cf89ce6c5390
jaeuk kim <wodnr76@naver.com>
2023-12-28 21:31:56 +0900

    Rename: 어노테이션 수정

--- src/main/java/com/getcode/domain/project/ProjectTech.java
+++ src/main/java/com/getcode/domain/project/ProjectTech.java
@@ -6,7 +6,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class ProjectTech {
 
 
---
+++ src/main/java/com/getcode/domain/projectrecruitment/ProjectRecruitmentTech.java
@@ -0,0 +1,2 @@
+package com.getcode.domain.projectrecruitment;public class ProjectRecruitmentTech {
+}
