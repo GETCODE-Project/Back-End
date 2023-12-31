2b5dc45781056b724668c1842d466edc1aba0c3c
jaeuk kim <wodnr76@naver.com>
2024-01-01 06:21:03 +0900

    Rename: 파일 수정

---
+++ src/main/java/com/getcode/controller/project/ProjectController.java
@@ -0,0 +1,4 @@
+package com.getcode.controller.project;
+
+public class ProjectController {
+}
--- src/main/java/com/getcode/domain/common/BaseTimeEntity.java
+++ src/main/java/com/getcode/domain/common/BaseTimeEntity.java
@@ -16,17 +16,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // BaseEntity를 상속한 엔티티들은 아래 필드들을 컬럼으로 인식하게 된다.
-@EntityListeners(AuditingEntityListener.class)  // Auditing(자동으로 값 매핑) 기능 추가
-public abstract class BaseTimeEntity {
-
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
-}
-@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
