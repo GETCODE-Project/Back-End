4ddd85360ed9d8529a52b0e730a1ac7a0a8067ed
KMK_HM <kyun9151@gmail.com>
2023-12-31 23:39:32 +0900

    feat: 일반 로그인 Swagger 작성

--- src/main/java/com/getcode/controller/MemberController.java
+++ src/main/java/com/getcode/controller/MemberController.java
@@ -37,9 +37,7 @@ public class MemberController {
 
     @Operation(summary = "일반 회원가입", description = "이메일: 기존 이메일 형식 / 닉네임: 2자 이상 / 비밀번호: 8자 이상")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "CREATED"),
-            @ApiResponse(responseCode = "404", description = "특정 유저가 존재하지 않음",
-                    content = @Content(schema = @Schema(implementation = BusinessExceptionResponse.class)))
+            @ApiResponse(responseCode = "201", description = "CREATED")
     })
     @PostMapping("/sign-up")
     public ResponseEntity<SignUpResponseDto> signup(@Valid @RequestBody SignUpDto signUpDto) {
@@ -48,13 +46,16 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
 
-
+    @Operation(summary = "일반 로그인", description = "로그인후, access/refresh Token 발행")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @PostMapping("/auth/login")
     public ResponseEntity<TokenDto> login(@RequestBody MemberLoginRequestDto memberRequestDto) {
         return ResponseEntity.ok(memberService.login(memberRequestDto));
     }
 
-    @Operation(summary = "사용자 개인 정보회회", description = "Acceess Token 인증 후, 사용자 개인정보 조회")
+    @Operation(summary = "사용자 개인정보 조회", description = "Acceess Token 인증 후, 사용자 개인정보 조회")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "202", description = "Accepted"),
     })
