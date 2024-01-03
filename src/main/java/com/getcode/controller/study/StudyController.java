6b60884c8773ce7796eaae629f6c2ed0760193d0
KMK_HM <kyun9151@gmail.com>
2024-01-03 23:02:37 +0900

    feat: 스터디 모집 Swagger 작성

--- src/main/java/com/getcode/controller/study/StudyController.java
+++ src/main/java/com/getcode/controller/study/StudyController.java
@@ -7,6 +7,9 @@ import com.getcode.dto.member.SignUpResponseDto;
 import com.getcode.dto.study.StudyRequestDto;
 import com.getcode.dto.study.StudyResponseDto;
 import com.getcode.service.study.StudyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StudyController {
     private final StudyService studyService;
+
+    @Operation(summary = "스터디 모집글 작성",
+            description = "제목: 2자 이상 / 내용: 2자 이상 / 지역: 필수 / 온라인 여부: 필수" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "CREATED")
+    })
     @PostMapping("/study")
     public ResponseEntity<StudyResponseDto> signup(@Valid @RequestBody StudyRequestDto req) {
         StudyResponseDto study = studyService.createStudy(req);
--- src/main/java/com/getcode/dto/study/StudyRequestDto.java
+++ src/main/java/com/getcode/dto/study/StudyRequestDto.java
@@ -2,6 +2,7 @@ package com.getcode.dto.study;
 
 import com.getcode.domain.member.Member;
 import com.getcode.domain.study.Study;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,15 +14,21 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyRequestDto {
+
+    @Schema(description = "제목", defaultValue = "Java 스터디")
     @NotBlank(message = "제목은 필수입니다.")
     @Length(min = 2, max = 15)
     private String title;
 
+    @Schema(description = "내용", defaultValue = "Java 스터디 모집합니다.")
     @NotBlank(message = "내용은 필수입니다.")
     @Length(min = 2, max = 1000)
     private String content;
 
+    @Schema(description = "지역", defaultValue = "서울")
     private String region;
+
+    @Schema(description = "온라인/오프라인", defaultValue = "true")
     private boolean online;
 
     public Study toEntity(Member member) {
