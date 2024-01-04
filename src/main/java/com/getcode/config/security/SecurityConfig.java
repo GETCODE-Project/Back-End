8ba383b3453873513c782d76035fa1f231cc12be
KMK_HM <kyun9151@gmail.com>
2024-01-05 00:03:15 +0900

    fix: Security 버전에 따른 설정 변경

--- src/main/java/com/getcode/config/security/SecurityConfig.java
+++ src/main/java/com/getcode/config/security/SecurityConfig.java
@@ -18,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -59,7 +60,7 @@ public class SecurityConfig {
 //                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(o -> o.userInfoEndpoint(u->u.userService(customOAuth2UserService))
                 .successHandler(oAuth2LoginSuccessHandler).failureHandler(oAuth2LoginFailureHandler))
-                .apply(new CustomFilterConfigurer());
+                .with(new CustomFilterConfigurer(), Customizer.withDefaults());
         return http.build();
     }
 
